46afdc1930e3be8f986a57e8b6e6c308a0314fd2
Arnur Nigmetov <anigmetov@lbl.gov>
2022-06-10 10:19:31 -0700

    Fix bug in __init__.py

--- bindings/python/hera/__init__.py
+++ bindings/python/hera/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import typing
+from icecream import ic
 
 # from ._hera import bottleneck_distance_exact_with_edge, bottleneck_distance_exact, bottleneck_distance_approx_with_edge, \
 #     bottleneck_distance_approx, WassersteinParams, WassersteinResult, DiagramPoint
@@ -35,20 +36,20 @@ def bottleneck_dist(dgm_a, dgm_b, delta: float = 0.01, return_bottleneck_edge: b
 #
 
 def wasserstein_cost_geom(pts_a: typing.List[typing.Tuple[float, float]], pts_b: typing.List[typing.Tuple[float, float]],
-                     params: typing.Optional[WassersteinParams] = None, prices=None):
+                     params: typing.Optional[WassersteinParams] = None, prices=None, return_detailed=False):
 
     if type(pts_a) is not list:
         pts_a = [ (x, y) for x, y in pts_a ]
     if type(pts_b) is not list:
-        pts_b = [ (x, y) for x, y in pts_a ]
+        pts_b = [ (x, y) for x, y in pts_b ]
     if params is None:
         params = WassersteinParams()
     if prices is None:
         prices = []
-    if not params.return_matching:
-        return wasserstein_cost_geom_(pts_a, pts_b, params, prices)
-    else:
+    if params.return_matching or return_detailed:
         return wasserstein_cost_geom_detailed_(pts_a, pts_b, params, prices)
+    else:
+        return wasserstein_cost_geom_(pts_a, pts_b, params, prices)
 
 
 def wasserstein_dist(dgm_a: typing.List[typing.Tuple[float, float]], dgm_b: typing.List[typing.Tuple[float, float]],
